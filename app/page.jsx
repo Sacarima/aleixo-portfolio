99299c498261a9cfede16b7747a29d757f60b98c
Sacarima <caroline12alex@yahoo.com>
2025-09-10 15:47:07 +0200

    update title

--- app/page.jsx
+++ app/page.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Engineer</span>
+            <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello, I'm <br /> <span className="text-[#00FF99]">João Aleixo</span>
             </h1>
